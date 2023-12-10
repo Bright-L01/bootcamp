158016f180bb3567317717eb4c2ce19fd8e2740c
Bright Liu <brightliu@college.harvard.edu>
2023-12-10 14:46:08 -0500

    wow

--- src/App.js
+++ src/App.js
@@ -2,44 +2,49 @@ import React from 'react';
 import CardEditor from './CardEditor';
 import CardViewer from './CardViewer';
 import Homepage from './Homepage';
-import PageRegister from './PageRegister';
-import PageLogin from './PageLogin';
+// import PageRegister from './PageRegister';
+// import PageLogin from './PageLogin';
 
 import { Switch, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { isLoaded } from 'react-redux-firebase';
+import { render } from 'react-dom';
+// import { connect } from 'react-redux';
+// import { isLoaded } from 'react-redux-firebase';
 
-const App = props => {
-  if (!isLoaded(props.auth, props.profile)) {
-    return <div>Authentication loading...</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards:[
+        {front: 'front1', back: 'back1'},
+        {front: 'front2', back: 'back2'},
+      ]
+    };
   }
 
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Homepage />
-      </Route>
-      <Route exact path="/editor">
-        <CardEditor />
-      </Route>
-      <Route exact path="/viewer/:deckId">
-        <CardViewer />
-      </Route>
-      <Route exact path="/register">
-        <PageRegister />
-      </Route>
-      <Route exact path="/login">
-        <PageLogin />
-      </Route>
-      <Route>
-        <div>Page not found!</div>
-      </Route>
-    </Switch>
-  );
-};
 
-const mapStateToProps = state => {
-  return { auth: state.firebase.auth, profile: state.firebase.profile };
-};
+  addCard = card => {
+      const cards = this.state.cards.slice().concat(card);
+      this.setState({ cards });
+  }
 
-export default connect(mapStateToProps)(App);
+  deleteCard = index => { 
+      const cards = this.state.cards.slice();
+      cards.splice(index, 1);
+      this.setState({ cards });
+  }
+
+  render () { 
+      return (
+          <Routes>
+
+              <Route exact path="/" component={Homepage} /> 
+              <Route exact path ="/editor" element = {<CardEditor cards={this.state.cards} addCard={this.addCard} deleteCard={this.deleteCard} />} />
+
+              <Route exact path ="/viewer" element = {<CardViewer cards={this.state.cards} />} />
+          </Routes>
+      );
+  } 
+
+}
+
+export default App;
--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,103 +1,60 @@
 import React from 'react';
-import './CardViewer.css';
-
-import { Link, withRouter } from 'react-router-dom';
-import {
-  firebaseConnect,
-  isLoaded,
-  isEmpty,
-  populate,
-} from 'react-redux-firebase';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import { Link, Router } from 'react-router-dom';
 
 class CardViewer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentIndex: 0,
-      displayFront: true,
-    };
-  }
-
-  nextCard = () => {
-    if (this.state.currentIndex < this.props.cards.length - 1) {
-      this.setState({
-        currentIndex: this.state.currentIndex + 1,
-        displayFront: true,
-      });
-    }
-  };
-
-  prevCard = () => {
-    if (this.state.currentIndex > 0) {
-      this.setState({
-        currentIndex: this.state.currentIndex - 1,
-        displayFront: true,
-      });
-    }
-  };
-
-  flipCard = () => this.setState({ displayFront: !this.state.displayFront });
+    constructor(props) {
+        super(props);
+        this.state = { front: '', back: '', 
+        currIndex : 0,
+        isFront: true
+        };
+      }
 
   render() {
-    if (!isLoaded(this.props.cards)) {
-      return <div>Loading...</div>;
-    }
-
-    if (isEmpty(this.props.cards)) {
-      return <div>Page not found!</div>;
-    }
-
-    // this uses the ternary operator:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
-    const card = this.props.cards[this.state.currentIndex][
-      this.state.displayFront ? 'front' : 'back'
-    ];
-
+    const card = this.props.cards[this.state.currIndex][this.state.isFront ? 'front' : 'back']
     return (
       <div>
-        <h2>{this.props.name}</h2>
-        <h4>Created by {this.props.username}</h4>
-        Card {this.state.currentIndex + 1} out of {this.props.cards.length}.
-        <div className="card" onClick={this.flipCard}>
-          {card}
-        </div>
-        <br />
-        <button
-          disabled={this.state.currentIndex === 0}
-          onClick={this.prevCard}
-        >
-          Prev card
+        <h2>Card Viewer</h2> 
+        <table>
+            <h2>
+                {card}
+            </h2> 
+        </table>
+        <button onClick={() => this.setState(prevState => ({ isFront: !prevState.isFront }))}>
+            flip
         </button>
-        <button
-          disabled={this.state.currentIndex === this.props.cards.length - 1}
-          onClick={this.nextCard}
-        >
-          Next card
+        <button onClick={() => this.setState(prevState => ({ currIndex: Math.max(prevState.currIndex - 1, 0) , isFront : true}))}>
+         Previous
         </button>
-        <hr />
-        <Link to="/">Home</Link>
+        <button onClick={() => this.setState(prevState => ({ currIndex: Math.min(this.props.cards.length - 1, prevState.currIndex + 1),  isFront : true }))}>
+         next
+        </button>
+        <h2>{this.state.currIndex+1} / {this.props.cards.length} </h2>
+
+        <Link to="/editor">Go to card viewer</Link>
       </div>
     );
   }
 }
 
-const populates = [{ child: 'owner', root: 'users' }];
+export default CardViewer;/* 
+access properties of the props
+card.front for whatever index we are on
+change what we see on the page<td>{false ? card.front : card.back}</td>
+isFront = true, on start be true<button onClick={() => this.deleteCard(index)}>
 
-const mapStateToProps = (state, props) => {
-  const deck = populate(state.firebase, props.match.params.deckId, populates);
-  const name = deck && deck.name;
-  const cards = deck && deck.cards;
-  const username = deck && deck.owner && deck.owner.username;
-  return { cards: cards, name: name, username: username };
-};
+key is the index of the card we want to change
+<tr key={index}>
+     {<td>{card.front}</td>}
+     <td>{card.back}</td>
+     <td>
 
-export default compose(
-  withRouter,
-  firebaseConnect(props => {
-    const deckId = props.match.params.deckId;
-    return [{ path: `/flashcards/${deckId}`, storeAs: deckId, populates }];
-  }),
-  connect(mapStateToProps),
-)(CardViewer);
+     </td>
+   </tr>
+
+
+Previous/Next:
+handleChange = event =>
+<button onClick={() => index - 1}>previous</button>
+<button onClick={() => index + 1}>Next</button>
+*/
